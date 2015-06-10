6b1f8a290dfee200669d266266737b9620739347
Guy Biton <guyb@wix.com>
2015-06-10 17:02:29 +0300

    adding project

---
+++ index.js
@@ -0,0 +1,51 @@
+
+var SpecReporter = function(baseReporterDecorator, formatError, config) {
+  baseReporterDecorator(this);
+
+  require('colors');
+
+  this.failures = [];
+  this.TOTAL_SUCCESS = '\n' + 'TOTAL: %d SUCCESS'.green;
+  this.TOTAL_FAILED = '\n' + 'TOTAL: %d FAILED'.red + ', ' +  '%d SUCCESS'.green;
+
+  this.onRunComplete = function(browsers, results) {
+    if (browsers.length >= 1 && !results.disconnected && !results.error) {
+      if (!results.failed) {
+        this.write(this.TOTAL_SUCCESS, results.success);
+      } else {
+        this.write(this.TOTAL_FAILED, results.failed, results.success);
+        this.logFinalErrors(this.failures);
+      }
+    }
+    this.write("\n");
+    this.failures = [];
+  };
+
+  this.logFinalErrors = function(errors) {
+    this.writeCommonMsg('\n\n') ;
+
+    errors.forEach(function(failure, index) {
+      index = index + 1;
+		this.writeCommonMsg((index + ') ').grey) ;
+		this.writeCommonMsg(('IT => ' + failure.description +'\n').cyan);
+		this.writeCommonMsg(('DESCRIBE => ' + failure.suite + '\n').yellow);
+		this.writeCommonMsg(('ERROR => ' + failure.log + '\n').red);
+    }, this);
+
+    this.writeCommonMsg('\n\n') ;
+  };
+
+  var reporterCfg = config.specReporter || {};
+  function noop(){}
+  this.onSpecFailure = function(browsers, results) {
+    this.failures.push(results);
+  };
+
+  this.specFailure = reporterCfg.suppressFailed ? noop : this.onSpecFailure;
+};
+
+SpecReporter.$inject = ['baseReporterDecorator', 'formatError', 'config'];
+
+module.exports = {
+  'reporter:wixKarmaReporter': ['type', SpecReporter]
+};
